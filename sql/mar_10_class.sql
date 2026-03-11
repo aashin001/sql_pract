359d7a16a01cb4e934e3f1fd0bdd37a3c31db860
aashin001 <115887524+aashin001@users.noreply.github.com>
2026-03-11 20:07:38 +0530

    Add files via upload

---
+++ sql/mar_10_class.sql
@@ -0,0 +1,11 @@
+-- TCL - commit, rollback, savepoint
+/*
+DDL, DML - in mysql its defult(autocommit on)
+DDL - is impossible to turn off the auto commit(commit- save)
+DML - possible to off the auto commit
+sessiom = commit to commit
+
+start transaction is for turning off the auto commit
+savepoint is not = commit
+
+*/
